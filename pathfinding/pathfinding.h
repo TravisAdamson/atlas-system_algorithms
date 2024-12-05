1e0fccc69d047c49a7ea402edfeed8355f01d919
Travis <travis.adamson@atlasschool.com>
2024-12-05 08:17:35 -0600

    Fixed prototype

--- pathfinding/pathfinding.h
+++ pathfinding/pathfinding.h
@@ -31,7 +31,7 @@ queue_t *dijkstra_graph(graph_t *graph, vertex_t const *start,
 						vertex_t const *target);
 
 /* helper prototypes */
-queue_t *backtrack(char **map, int rows, int cols,
+queue_t *recur_back(char **map, int rows, int cols,
 					int x, int y, point_t const *target,
-					queue_t *que, int *visit);
+					queue_t *new_que, int *current_visit);
 #endif
